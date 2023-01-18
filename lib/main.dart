b8558f255028c66fff27275d689a20edd10ee78d
mvharikrishnan <harikrish.hmv@gmail.com>
2023-01-18 14:20:48 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_api/View/home.dart';
+
+void main(List<String> args) {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
